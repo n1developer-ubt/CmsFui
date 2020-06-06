604dc47f0c41338a35ba41da6decd94813fea641
Usama Tariq <usamat37@gmail.com>
2020-06-06 05:16:45 -0700

    Updated

--- CmsFui/CmsFui/Controls/PrimaryButton.cs
+++ CmsFui/CmsFui/Controls/PrimaryButton.cs
@@ -7,8 +7,8 @@ namespace CmsFui.Controls
 {
     class PrimaryButton:Button
     {
-        public Color OnPressColor { get; set; }
-        public Color OnReleaseColor { get; set; }
+        public Color OnPressBackgroundColor { get; set; }
+        public Color OnReleaseBackgroundColor { get; set; }
         public Color OnReleaseTextColor { get; set; }
         public Color OnPressTextColor { get; set; }
         
@@ -16,18 +16,20 @@ namespace CmsFui.Controls
         {
             this.Pressed += OnPressed;
             this.Released += OnReleased;
-            OnReleaseColor = BackgroundColor;
-            OnPressColor = BackgroundColor;
+            OnReleaseBackgroundColor = BackgroundColor;
+            OnReleaseTextColor = TextColor;
+            OnPressTextColor = TextColor;
+            OnPressBackgroundColor = BackgroundColor;
         }
 
         private void OnReleased(object sender, EventArgs e)
         {
-            BackgroundColor = OnReleaseColor;
+            BackgroundColor = OnReleaseBackgroundColor;
         }
 
         private void OnPressed(object sender, EventArgs e)
         {
-            BackgroundColor = OnPressColor;
+            BackgroundColor = OnPressBackgroundColor;
         }
     }
 }
