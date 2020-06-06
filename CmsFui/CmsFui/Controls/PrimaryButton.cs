dd3403460b60b32bd4874bc4d1018bc837fc4fab
Usama Tariq <usamat37@gmail.com>
2020-06-06 05:08:40 -0700

    Add project files.

---
+++ CmsFui/CmsFui/Controls/PrimaryButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CmsFui.Controls
+{
+    class PrimaryButton:Button
+    {
+        public Color OnPressColor { get; set; }
+        public Color OnReleaseColor { get; set; }
+        public Color OnReleaseTextColor { get; set; }
+        public Color OnPressTextColor { get; set; }
+        
+        public PrimaryButton() : base()
+        {
+            this.Pressed += OnPressed;
+            this.Released += OnReleased;
+            OnReleaseColor = BackgroundColor;
+            OnPressColor = BackgroundColor;
+        }
+
+        private void OnReleased(object sender, EventArgs e)
+        {
+            BackgroundColor = OnReleaseColor;
+        }
+
+        private void OnPressed(object sender, EventArgs e)
+        {
+            BackgroundColor = OnPressColor;
+        }
+    }
+}
