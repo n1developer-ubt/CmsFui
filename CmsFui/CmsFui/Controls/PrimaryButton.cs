10e63c2bbd09c73100f03a9bb3babced4f28084f
Usama Tariq <usamat37@gmail.com>
2020-06-06 07:36:24 -0700

    Updated

--- CmsFui/CmsFui/Controls/PrimaryButton.cs
+++ CmsFui/CmsFui/Controls/PrimaryButton.cs
@@ -7,29 +7,51 @@ namespace CmsFui.Controls
 {
     class PrimaryButton:Button
     {
-        public Color OnPressBackgroundColor { get; set; }
-        public Color OnReleaseBackgroundColor { get; set; }
-        public Color OnReleaseTextColor { get; set; }
-        public Color OnPressTextColor { get; set; }
-        
+        public static readonly BindableProperty OnPressBackgroundColorProperty = BindableProperty.Create("OnPressBackgroundColor", typeof(Color), typeof(VisualElement), Color.Black);
+        public static readonly BindableProperty OnReleaseBackgroundColorProperty = BindableProperty.Create("OnReleaseBackgroundColor", typeof(Color), typeof(VisualElement), Color.Black);
+        public static readonly BindableProperty OnReleaseTextColorProperty = BindableProperty.Create("OnReleaseTextColor", typeof(Color), typeof(VisualElement), Color.Black);
+        public static readonly BindableProperty OnPressTextColorProperty = BindableProperty.Create("OnPressTextColor", typeof(Color), typeof(VisualElement), Color.Black);
+
+        public Color OnPressBackgroundColor
+        {
+            get=>(Color)GetValue(OnPressBackgroundColorProperty); 
+            set=>SetValue(OnPressBackgroundColorProperty, value);
+        }
+
+        public Color OnReleaseBackgroundColor
+        {
+            get => (Color)GetValue(OnReleaseBackgroundColorProperty);
+            set => SetValue(OnReleaseBackgroundColorProperty, value);
+        }
+
+        public Color OnReleaseTextColor
+        {
+            get => (Color)GetValue(OnReleaseTextColorProperty);
+            set => SetValue(OnReleaseTextColorProperty, value);
+        }
+
+        public Color OnPressTextColor
+        {
+            get => (Color)GetValue(OnPressTextColorProperty);
+            set => SetValue(OnPressTextColorProperty, value);
+        }
+
         public PrimaryButton() : base()
         {
             this.Pressed += OnPressed;
             this.Released += OnReleased;
-            OnReleaseBackgroundColor = BackgroundColor;
-            OnReleaseTextColor = TextColor;
-            OnPressTextColor = TextColor;
-            OnPressBackgroundColor = BackgroundColor;
         }
 
         private void OnReleased(object sender, EventArgs e)
         {
             BackgroundColor = OnReleaseBackgroundColor;
+            TextColor = OnReleaseTextColor;
         }
 
         private void OnPressed(object sender, EventArgs e)
         {
             BackgroundColor = OnPressBackgroundColor;
+            TextColor = OnPressTextColor;
         }
     }
 }
